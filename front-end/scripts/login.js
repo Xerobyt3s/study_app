43fdc8e37b562dfd588e56142d7c6d83f0cfb9ef
Sebastian <sebastian.hallander.gbg22@amerikanskagymnasiet.se>
2023-09-13 00:43:37 +0200

    login-page

---
+++ front-end/scripts/login.js
@@ -0,0 +1,9 @@
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+    console.log(username + " " + password)
+})
