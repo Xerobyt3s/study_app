4eb445d6d6d2b4177eb06bf23231bbb3d2466cec
xero <letsdoit3691@gmail.com>
2023-09-13 14:09:56 +0200

    system for sending data to server
    still has issue connecting

--- front-end/scripts/login.js
+++ front-end/scripts/login.js
@@ -1,9 +1,20 @@
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 
+socket = new WebSocket("ws://10.31.13.77:9993");
+
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
     const username = loginForm.username.value;
     const password = loginForm.password.value;
-    console.log(username + " " + password)
+    socket.send(username + " " + password);
 })
+
+socket.addEventListener("open", (e) => {
+    e.preventDefault();
+    console.log("connected")
+  });
+
+socket.onopen = function () {
+    
+};
