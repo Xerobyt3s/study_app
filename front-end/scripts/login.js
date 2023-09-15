26de61fb4f7b748ee4ad56a264d7153c6ad3a765
Sebatian-H <sebastian.hallander.gbg22@amerikanskagymnasiet.se>
2023-09-16 00:19:17 +0200

    javascript cookies

--- front-end/scripts/login.js
+++ front-end/scripts/login.js
@@ -1,32 +1,34 @@
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 
-const address = "localhost"
-const port = 8001
+const address = "localhost";
+const port = 8001;
 
 var socket = new WebSocket(`ws://${address}:${port}`);
 
-var auth_status
-var username
-var password
+var auth_status;
+var username;
+var password;
 
 //Send username and password entered in html form when login button is clicked
-loginButton.addEventListener("click", async e => {
-    e.preventDefault();
-    const username = loginForm.username.value;
-    const password = loginForm.password.value;
-    const event = {type: "auth", username: username, password: password}
-    socket.send(JSON.stringify(event));
-})
+loginButton.addEventListener("click", async (e) => {
+  e.preventDefault();
+  username = loginForm.username.value;
+  password = loginForm.password.value;
+  const event = { type: "auth", username: username, password: password };
+  socket.send(JSON.stringify(event));
+});
 //If connected to websocket send "connection established" to websocket
 socket.addEventListener("open", (e) => {
-    console.log("connection established")
-  });
+  console.log("connection established");
+});
 //When message is recieved it is printed to the console
 socket.addEventListener("message", (e) => {
-  auth_status = e.data
-  console.log(auth_status)
-  if (auth_status == "completed"){
-    window.location.href = "../html/main.html"
+  auth_status = e.data;
+  console.log(auth_status);
+  if (auth_status == "completed") {
+    document.cookie = `Username=Admin`
+    document.cookie = `Password=Admin`
+    window.location.href = "../html/main.html";
   }
-})
+});
--- front-end/scripts/menu.js
+++ front-end/scripts/menu.js
@@ -1,2 +1,39 @@
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let clist = decodedCookie.split(";");
+  for (i in clist) {
+    let c = clist[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
 
+console.log(document.cookie)
 
+let username = getCookie("Username");
+let password = getCookie("Password");
+console.log(`username is ${username} and password is ${password}`)
+
+const address = "localhost";
+const port = 8001;
+
+var socket = new WebSocket(`ws://${address}:${port}`);
+
+socket.addEventListener("open", (e) => {
+  const event = { type: "auth", username: username, password: password };
+  socket.send(JSON.stringify(event));
+});
+
+socket.addEventListener("message", (e) => {
+  auth_status = e.data;
+  console.log(auth_status);
+  if (auth_status == "failed") {
+    window.location.href = "../";
+  }
+});
